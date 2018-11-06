9e3b82e25bc3bc4ad1cf72fde266e2147818a432
s.bautista <s.bautista@ph.fujitsu.com>
2018-11-06 15:57:57 +0800

    [Modified] Put the working code for Part 3 of Week 2 Assignment

--- corr.R
+++ corr.R
@@ -3,13 +3,42 @@
 corr <- function(directory, threshold = 0) {
 	## 'directory' is a character vector of length indicating
 	## the location of the CSV files
+	listOfCSV <- as.character(list.files(directory))
 	
-	## 'threshold' is a numeric vector of length 1 indicating the
-	## number of completely ibserved (on all variables)
-	## required to compute the correlation between
-	## nitrate and sulfate; the default is 0
+	# Checks if the file in the directory exist
+	specDataExist <- dir.exists(directory)
+	
+	# If spec data folder exist change the directory structure
+	if(specDataExist == TRUE) {
+		directory <- './specdata/'
+	}
+	
+	# Create a path for each of the files in the folder
+	filePaths <- paste(directory, listOfCSV, sep="")
+	
+	# Store the data
+	corrValue = NULL
+	
+	# Loop the files
+	for(filesToRead in 1:332) {
+		# Reads each file
+		readCSVFiles <- read.csv(filePaths[filesToRead], header=TRUE)
+		
+		# Removes NA's or any missing value by using complete.cases()
+		# Create it by row
+		completeValues = readCSVFiles[complete.cases(readCSVFiles), ]
+		
+		## 'threshold' is a numeric vector of length 1 indicating the
+		## number of completely observed (on all variables)
+		## required to compute the correlation between
+		## nitrate and sulfate; the default is 0
+		
+		if(nrow(completeValues) > threshold) {
+			corrValue <- c(corrValue, cor(completeValues[,"sulfate"], completeValues[, "nitrate"]))
+		}
+	}
 	
 	## Return a numeric vector of correlations
 	## NOTE: Do not round the result!
-	
+	return (corrValue)
 }
