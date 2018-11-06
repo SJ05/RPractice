ccc6f99601f17a1333993e4592691fd28b8a7982
s.bautista <s.bautista@ph.fujitsu.com>
2018-11-06 13:16:36 +0800

    [Update] part 2 of assignment for week 2

--- complete.R
+++ complete.R
@@ -2,14 +2,45 @@
 complete <- function(directory, id = 1:332) {
         ## 'directory' is a character vector of length indicating
         ## the location of the CSV files
-        
+		listOfCSV <- as.character(list.files(directory))
+		
+		# Checks if the file in the directory exist
+		specDataExist <- dir.exists(directory)
+		
+		# If spec data folder exist change the directory structure
+		if(specDataExist == TRUE) {
+			directory <- './specdata/'
+		}
+		
+		# Create a path for each of the files in the folder
+		filePaths <- paste(directory, listOfCSV, sep="")
+		
+		# Store the data
+		data <- data.frame()
+		
         ## 'id' is an integer vector indicating the monitor ID numbers
         ## to be used
-        
-        ## Return a data frame of the form:
-        ## id nobs
-        ## 1  117
-        ## 2  1041
-        ## ...
-        ## where 'id' is the monitor ID number and 'nobs is the number of complete cases
+		for(filesTobeRead in id) {
+			# Reads each file that the user selected
+			readCSVFiles <- read.csv(filePaths[filesTobeRead], header=TRUE)
+			
+			# Removes NA's or any missing value by using complete.cases()
+			# See https://stackoverflow.com/questions/4862178/remove-rows-with-all-or-some-nas-missing-values-in-data-frame
+			# Count the rows which do not contain 'NA' by using sum()
+			countCompleteValues <- sum(complete.cases(readCSVFiles))
+			
+			# Create a data frame where the file ids and valid values is shown
+			completeData <- data.frame(id = filesTobeRead, nobs = countCompleteValues)
+			
+			# Use rbind. It will show the values in rows
+			data <- rbind(data, completeData)
+		}
+		
+		## Return a data frame of the form:
+		## id nobs
+		## 1  117
+		## 2  1041
+		## ...
+		## where 'id' is the monitor ID number and 'nobs is the number of complete cases
+		return (data)
 }
