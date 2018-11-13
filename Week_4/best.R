4c9429c97143857a58ef7b340a68582aee53e769
s.bautista <s.bautista@ph.fujitsu.com>
2018-11-14 07:39:33 +0800

    [Add] Initial file for week4 assignment best.R

---
+++ Week_4/best.R
@@ -0,0 +1,11 @@
+# Write a function called best that take two arguments: the 2-character 
+# abbreviated name of a state and an outcome name.
+
+best <- function(state, outcome) {
+	## Read outcome data
+	
+	## Check that state and outcome are valid
+	
+	## Return hospital name in that state with lowest 30-day death
+	## rate
+}
